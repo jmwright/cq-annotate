ba0fa58706f551076bd5f04248f15955e4766c27
Jeremy Wright <wrightjmf@gmail.com>
2023-12-08 16:49:24 -0500

    Black formatting pass on examples

--- examples/circular_dimensions_example.py
+++ examples/circular_dimensions_example.py
@@ -3,9 +3,7 @@ from cq_annotate.dimensioning import add_circular_dimensions
 
 # Create a basic object that we will add a dimension to
 bd = cq.Workplane("YZ").circle(10.0).circle(5.0).extrude(50.0)
-bd.edges("%CIRCLE").edges(cq.selectors.RadiusNthSelector(1)).edges(">X").tag(
-    "radius_1"
-)
+bd.edges("%CIRCLE").edges(cq.selectors.RadiusNthSelector(1)).edges(">X").tag("radius_1")
 
 # Add the radius dimension
 assy = add_circular_dimensions(bd, arrow_scale_factor=0.1)
